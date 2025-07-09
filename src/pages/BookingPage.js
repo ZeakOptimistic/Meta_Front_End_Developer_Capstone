148c8ceb4360dbde336eaaf80e87b520945171b6
Zeak Optimistic <quangkhaint2005@gmail.com>
2025-07-09 07:11:14 +0700

    Refactor app into components and set up routing

---
+++ src/pages/BookingPage.js
@@ -0,0 +1,9 @@
+function BookingPage() {
+  return (
+    <main>
+      <h2>Reserve a Table</h2>
+      <p>Booking content will go here.</p>
+    </main>
+  );
+}
+export default BookingPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <main>
+      <h2>Welcome to Little Lemon</h2>
+      <p>This is the homepage.</p>
+    </main>
+  );
+}
+export default HomePage;
